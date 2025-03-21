b98cae530f82f770f4846a2a698e2fe67c0a673b
Youn Mélois <youn@melois.dev>
2025-03-21 15:20:22 +0100

    add base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/isen-weather-app-no-rs-sadge/", // Base path for GitHub Pages
   server: {
     proxy: {
       "/api": {
